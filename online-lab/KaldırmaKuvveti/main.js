8027edad71f694bfc0b6c822da4000e6751b6e42
Emine <emine.ayranci@stu.fsm.edu.tr>
2020-06-01 03:29:39 +0300

    ilk deney

---
+++ online-lab/KaldırmaKuvveti/main.js
@@ -0,0 +1,4 @@
+function play() {
+    var audio = new Audio('Bu-1-gramlık-taştır-atmak-içi1590788848.mp3');
+    audio.play();
+  }
--- online-lab/main.js
+++
@@ -1,4 +0,0 @@
-function play() {
-    var audio = new Audio('Bu-1-gramlık-taştır-atmak-içi1590788848.mp3');
-    audio.play();
-  }
